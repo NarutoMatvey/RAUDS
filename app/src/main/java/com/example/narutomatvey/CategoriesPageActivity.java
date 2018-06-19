17a175d952791c97f42905d1cf086f0cc7e0350d
NarutoMatvey <NarutoMatvey@mail.ru>
2018-06-19 23:00:23 +0300

    Добавил функцию для отображения котегорий. Part 1.

--- app/src/main/java/com/example/narutomatvey/CategoriesPageActivity.java
+++ app/src/main/java/com/example/narutomatvey/CategoriesPageActivity.java
@@ -10,11 +10,13 @@ import android.widget.TextView;
 
 public class CategoriesPageActivity extends AppCompatActivity {
 
-    LinearLayout llCategory;
+    LinearLayout llFLCategory, llNLCategory, llLLCategory;
     EditText editAddCategory;
     ImageButton imBtnAddCategory;
     LinearLayout.LayoutParams lParams;
-    String mySt = "Категория 1";
+    String[] stringCateg = {"Категория 1", "Категория 2", "Категория 3",
+                            "Категория 4", "Категория 5", "Категория 6",
+                            "Категория 7", "Категория 8", "Категория 9"};
     final String LOG_TAG = "myLogs";
 
     @Override
@@ -22,7 +24,11 @@ public class CategoriesPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories_page);
 
-        llCategory =  findViewById(R.id.llCategory);
+
+        llFLCategory =  findViewById(R.id.firstLineCategory);
+        llNLCategory =  findViewById(R.id.nextLineCategory);
+        llLLCategory = findViewById(R.id.lastLineCategory);
+
         editAddCategory = findViewById(R.id.editAddCategory);
         imBtnAddCategory = findViewById(R.id.imBtnAddCategory);
         Log.d(LOG_TAG, "Нашёл компоненты");
@@ -36,9 +42,20 @@ public class CategoriesPageActivity extends AppCompatActivity {
         Log.d(LOG_TAG, "---------------------------------");
 
         lParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        TextView textCategory = new TextView(this);
-        textCategory.setText(mySt);
-        llCategory.addView(textCategory, lParams);
+
+        for(Integer i=0; i < stringCateg.length; i++) {
+            TextView textCategory = new TextView(this);
+            textCategory.setText(stringCateg[i]);
+            if( 0 <= i % 9 && i % 9 <= 2 ){
+                llFLCategory.addView(textCategory, lParams);
+            }
+            else if( 3 <= i % 9 && i % 9 <= 5 ){
+                llNLCategory.addView(textCategory, lParams);
+            }
+            else if( 6 <= i % 9 && i % 9 <= 8 ){
+                llLLCategory.addView(textCategory, lParams);
+            }
+        }
     }
 }
 
